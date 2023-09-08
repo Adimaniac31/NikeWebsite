66b97d80fdf74d200d0fdf6bd0d11d129afa5cba
adimaniac <adityabhigade31@gmail.com>
2023-09-08 15:35:33 +0530

    create subscribe component and import it to index.js

--- src/sections/index.js
+++ src/sections/index.js
@@ -5,3 +5,4 @@ import PopularProduct from "./PopularProduct";
 import Services from "./Services";
 import SpecialOffer from "./SpecialOffer";
 import SuperQuality from "./SuperQuality";
+import subscribe from "./subscribe";
