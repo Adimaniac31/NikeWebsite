8576698767cc66c6aed8ab79e9a868723e7e3424
adimaniac <adityabhigade31@gmail.com>
2023-09-08 15:32:43 +0530

    start project and do some basic work on it by installing tailwind

---
+++ src/sections/index.js
@@ -0,0 +1,7 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
