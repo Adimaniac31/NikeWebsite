2d4f09733afdfd6c624458eb90d622e8d35d7eb6
adimaniac <adityabhigade31@gmail.com>
2023-09-08 15:39:18 +0530

    change name of subscribe to Subscribe and add it to index.js

--- src/sections/index.js
+++ src/sections/index.js
@@ -5,4 +5,15 @@ import PopularProduct from "./PopularProduct";
 import Services from "./Services";
 import SpecialOffer from "./SpecialOffer";
 import SuperQuality from "./SuperQuality";
-import subscribe from "./subscribe";
+import Subscribe from "./Subscribe";
+
+export{
+    Hero,
+    CustomerReviews,
+    PopularProduct,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    Subscribe,
+    Footer
+}
