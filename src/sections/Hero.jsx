0c3543eb1ab7f3b6dda8343b734cb27b59df44b4
adimaniac <adityabhigade31@gmail.com>
2023-09-09 20:38:08 +0530

    add some styling to Hero Component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,14 +9,13 @@ const Hero = () => {
     className='w-full flex xl:flex-row flex-col justify-center min-h-screen border-2 border-red-500 p-2'
     >
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
-        <p>Our Summer Collections</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p className='text-xl font-palanquin text-blue-400' >Our Summer Collections</p>
+        <h1 className= "text-md font-palanquin text-slate-400 mt-10 text-4xl max-sm:leading-[82] font-bold">
+          <span className='max-sm:72px xl:bg-white xl:white-space-nowrap relative z-10 pr-10 inline-block mt-3'>The New Arrival</span>
           <br />
-          <span>Nike</span>
-          Shoes 
+          <span className="text-8xl text-coral-red font-montserrat ">Nike</span> Shoes 
         </h1>
-        <p>
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mb-14'>
           Discover stylish Nike
           Arrivals, quality comfort, 
           and innovation for your 
@@ -27,7 +26,7 @@ const Hero = () => {
                         w-full mt-20 gap-16'>
                           {statistics.map((stat,index)=>(
                             <div>
-                              <p className='font-montserrat text-2xl text-zinc-300'>{stat.value}</p>
+                              <p className='font-montserrat text-4xl text-zinc-300 font-bold'>{stat.value}</p>
                               <pc className='font-palanquin text-lg'>{stat.label}</pc>
                             </div>
                           ))}
