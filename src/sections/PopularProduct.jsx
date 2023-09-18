36933264fcb86a0a25f2d44bceb4391a16b3a206
adimaniac <adityabhigade31@gmail.com>
2023-09-18 20:59:50 +0530

    add styling to popular

--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -5,7 +5,7 @@ const PopularProduct = () => {
   return (
     <section id="products" className='max-container max-sm:mt-12'>
       <div className='flex flex-col justify-start gap-5'>
-        <h2 className='text-4xl font-palanquin font-bold'><span>Popular</span>Products</h2>
+        <h2 className='text-4xl font-palanquin font-bold'><span className='text-coral-red'>Popular</span>Products</h2>
         <p>
           We have a wide range of quality and affordable products to suit your needs. We are committed towards providing you with the best possible service at an
         </p>
