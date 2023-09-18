86c29ade3779bcec883f7d85165b51123fd743b4
adimaniac <adityabhigade31@gmail.com>
2023-09-18 20:57:34 +0530

    add some details in div

--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -4,8 +4,11 @@ import { products } from "../Constants"
 const PopularProduct = () => {
   return (
     <section id="products" className='max-container max-sm:mt-12'>
-      <div className='flex flex-col'>
-      
+      <div className='flex flex-col justify-start gap-5'>
+        <h2 className='text-4xl font-palanquin font-bold'><span>Popular</span>Products</h2>
+        <p>
+          We have a wide range of quality and affordable products to suit your needs. We are committed towards providing you with the best possible service at an
+        </p>
       </div>
     </section>
   )
