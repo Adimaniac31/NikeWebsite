0b1ee0b2d92763f7155114464cae17890252df01
adimaniac <adityabhigade31@gmail.com>
2023-09-18 21:01:24 +0530

    add styling to paragraph

--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -6,7 +6,7 @@ const PopularProduct = () => {
     <section id="products" className='max-container max-sm:mt-12'>
       <div className='flex flex-col justify-start gap-5'>
         <h2 className='text-4xl font-palanquin font-bold'><span className='text-coral-red'>Popular</span>Products</h2>
-        <p>
+        <p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>
           We have a wide range of quality and affordable products to suit your needs. We are committed towards providing you with the best possible service at an
         </p>
       </div>
