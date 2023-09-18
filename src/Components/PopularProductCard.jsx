9760a78129a666e8138e59222754f15491173515
adimaniac <adityabhigade31@gmail.com>
2023-09-18 21:13:06 +0530

    add images

--- src/Components/PopularProductCard.jsx
+++ src/Components/PopularProductCard.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const PopularProductCard = ({imgURL,name,price}) => {
   return (
-    <div>PopularProductCard</div>
+    <div className='flex flex-1 flex-col w-full max-sm:w-full'>
+        <img 
+        src={imgURL}
+        alt={name}
+        className='w-280px h-[280px]'
+        />
+    </div>
   )
 }
 
