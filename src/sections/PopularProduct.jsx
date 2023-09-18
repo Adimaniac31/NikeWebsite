37d87679c4aa0bd1a9f9589734774c1185a28444
adimaniac <adityabhigade31@gmail.com>
2023-09-18 20:54:06 +0530

    add styling to produc section

--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -3,8 +3,10 @@ import { products } from "../Constants"
 
 const PopularProduct = () => {
   return (
-    <section id="products">
+    <section id="products" className='max-container max-sm:mt-12'>
+      <div className='flex flex-col'>
       
+      </div>
     </section>
   )
 }
