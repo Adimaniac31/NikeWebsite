fc712bf47bae4c9fac1b7088002da615893efa38
adimaniac <adityabhigade31@gmail.com>
2023-09-21 17:56:43 +0530

    delete earlier changes and copy p tag from hero section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -8,11 +8,19 @@ const SuperQuality = () => {
       items-center max-lg:flex-col gap-10
       w-ull max-container'
     >
-    <div className='flex flex-1 flex-col'>
-      <p>Discover Stylish Nike arrivals, Quality comfort, and
-      innovation for your life</p>
-    </div>
-
+             <p className='text-xl font-palanquin text-blue-400' >Our Summer Collections</p>
+        <h1 className= "text-md font-palanquin text-slate-400 mt-10 text-4xl max-sm:leading-[82] font-bold">
+          <span className='max-sm:72px xl:bg-white xl:white-space-nowrap relative z-10 pr-10 inline-block mt-3'>The New Arrival</span>
+          <br />
+          <span className="text-8xl text-coral-red font-montserrat ">Nike</span> Shoes 
+        </h1>
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mb-14'>
+          Discover stylish Nike
+          Arrivals, quality comfort, 
+          and innovation for your 
+          active life.
+        </p>
+        <Button label="Shop Now" iconURL={arrowRight}/>
     </section>
   )
 }
