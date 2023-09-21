927d8747f695d16e843c977e6897a85a899a49ec
adimaniac <adityabhigade31@gmail.com>
2023-09-21 17:52:19 +0530

    create a div and add text to it

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -8,6 +8,10 @@ const SuperQuality = () => {
       items-center max-lg:flex-col gap-10
       w-ull max-container'
     >
+    <div className='flex flex-1 flex-col'>
+      <p>Discover Stylish Nike arrivals, Quality comfort, and
+      innovation for your life</p>
+    </div>
 
     </section>
   )
